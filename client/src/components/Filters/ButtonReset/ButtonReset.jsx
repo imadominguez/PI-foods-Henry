bc0581afe6dbb237055b9edd6501e34376164949
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-01-22 16:41:11 -0300

    estilado boton reset

--- client/src/components/Filters/ButtonReset/ButtonReset.jsx
+++ client/src/components/Filters/ButtonReset/ButtonReset.jsx
@@ -1,5 +1,6 @@
 import { useDispatch } from "react-redux";
 import * as action from "../../../redux/actions";
+import s from "./ButtonReset.module.css";
 
 const ButtonReset = () => {
   const dispatch = useDispatch();
@@ -9,7 +10,7 @@ const ButtonReset = () => {
   };
   return (
     <div>
-      <button type="reset" onClick={handleReset}>
+      <button type="reset" onClick={handleReset} className={s.reset__link}>
         Reset
       </button>
     </div>

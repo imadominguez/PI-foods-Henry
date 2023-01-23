40de214f9374d679ddc08206c9fd2643cb299db6
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-01-23 15:55:48 -0300

    Componente detail recibe todos los datos OK

--- client/src/pages/Detail/Detail.jsx
+++ client/src/pages/Detail/Detail.jsx
@@ -1,7 +1,35 @@
-const Detail = (props) => {
+import { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+const { default: axios } = require("axios");
+
+const Detail = () => {
+  const { id, dataBase } = useParams();
+  const [detailrecipe, setdetailrecipe] = useState({});
+
+  const getDetail = async (id, dataBase) => {
+    const response = await axios.get(
+      `http://localhost:3001/recipes/${id}/${dataBase}`
+    );
+    setdetailrecipe(response.data);
+  };
+  console.log(detailrecipe);
+  const parseoHtml = (text) => {
+    const pattern = /<[^>]*>/gi;
+    const parseo = text?.replace(pattern, "");
+    return parseo;
+  };
+  useEffect(() => {
+    async function getRecipe() {
+      await getDetail(id, dataBase);
+    }
+    getRecipe();
+  }, []);
   return (
-    <div>
-      <h1>Hola estas en detail</h1>
+    <div style={{ color: "white" }}>
+      <h2>{detailrecipe.title}</h2>
+      <h1>{detailrecipe.id}</h1>
+      <p>{parseoHtml(detailrecipe?.summary)}</p>
     </div>
   );
 };
